c0b364925f84bda3a9e0a44b724931e5d2ccf247
shiruimeng <874237765@qq.com>
2020-07-15 18:49:44 +0800

    20200715_add LoginPage.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="appCss">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
@@ -17,7 +17,7 @@ export default {
 </script>
 
 <style>
-#app {
+#appCss {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
---
+++ src/components/LoginPage.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+        <span>
+            用户名：
+        </span>
+        <input name="username" placeholder="请输入用户名" type="text" v-model="LoginForm.username"/>
+        <br/><br/>
+        <span>
+            密码：
+        </span>
+        <input name="password" placeholder="请输入用户名" type="text" v-model="LoginForm.password"/>
+        <br/><br/>
+        <button name="submit" v-on:click="login" >登录</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "LoginPage",
+        data (){
+            return {
+                LoginForm : {
+                    username : '',
+                    password : ''
+                },
+                responseResult : []
+            }
+        },
+        methods: {
+            login (){
+                this.$axios.port('/login',
+                    {
+                        username: this.LoginForm.username,
+                        password: this.LoginForm.password
+                    }
+                ).then(
+                    successResponse =>{
+                        if(successResponse.data().return_code === 200){
+                            this.$router.replace({path:'/index'})
+                        }
+                    }
+                ).catch(failResponse =>{
+
+                })
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
